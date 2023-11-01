2b93dfe99ef6265f093244651d6db6affaab8d70
liu-zhibo <liuzhibo055@nuaa.edu.cn>
2023-11-01 09:55:23 +0800

    add NN models

---
+++ model/rnn.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+
+
+class RNN(nn.Module):
+    def __init__(self, input_shape, output_shape, rnn_hidden_size=64):
+        super(RNN, self).__init__()
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.rnn_hidden_size = rnn_hidden_size
+
+        self.fc1 = nn.Linear(input_shape, rnn_hidden_size)
+        self.rnn = nn.GRUCell(rnn_hidden_size, rnn_hidden_size)
+        self.fc2 = nn.Linear(rnn_hidden_size, output_shape)
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, obs, hidden_state=None):
+        input = obs.view(-1, self.input_shape).to(torch.float32)
+        x = f.relu(self.fc1(input))
+        h_in = hidden_state.reshape(-1, self.rnn_hidden_size)
+        h = self.rnn(x, h_in)
+        q = self.fc2(h)
+        return q, h
